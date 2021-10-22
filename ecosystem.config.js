717c7f52f1bb526ba0c166715f50efeaadab7633
xutongbao <1183391880@qq.com>
2021-10-22 14:01:40 +0800

    优化

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,21 @@
-module.exports = {
-  apps: [
-    {
-      name: `${process.env.name}`,
-      script: './app.js',
-      watch: process.env.watch === 'false' ? false : true,
-      ignore_watch: ['node_modules', 'log', 'jenkins', 'dbFile', '.git', 'prettylist.js', 'prettylist.txt', 'port.js', 'portUsed.txt'],
-      node_args: ['--inspect'],
-      env_development: {
-        PORT: 81,
-        NODE_ENV: 'development'
-      },
-      env_production: {
-        PORT: 80,
-        NODE_ENV: 'production'
-      }
+const apps = [
+  {
+    name: `${process.env.name}`,
+    script: './app.js',
+    watch: process.env.watch === 'false' ? false : true,
+    ignore_watch: ['node_modules', 'log', 'jenkins', 'dbFile', '.git', 'prettylist.js', 'prettylist.txt', 'port.js', 'portUsed.txt'],
+    node_args: ['--inspect'],
+    env_development: {
+      PORT: 81,
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      PORT: 80,
+      NODE_ENV: 'production'
     }
-  ]
+  }
+]
+
+export {
+  apps
 }
